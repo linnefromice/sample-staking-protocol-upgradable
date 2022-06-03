d1ffb31ec5d4acdee1dd33c55fef430422dd0965
linnefromice <linnefromice@gmail.com>
2022-06-03 22:19:39 +0900

    update: hardhat.config - import @openzeppelin/hardhat-upgrades

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import '@nomiclabs/hardhat-ethers'
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
+import "@openzeppelin/hardhat-upgrades"
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import { HardhatUserConfig } from "hardhat/config";
