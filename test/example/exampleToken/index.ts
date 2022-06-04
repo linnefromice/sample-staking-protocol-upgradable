3572f68091fb6a64c985011e5a3c0c84a68f8c2b
linnefromice <linnefromice@gmail.com>
2022-06-04 16:54:37 +0900

    update: expand test to upgrade & add v2 contract to use for upgrading

--- test/example/exampleToken/index.ts
+++ test/example/exampleToken/index.ts
@@ -1,7 +1,8 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { expect } from "chai"
+import { ContractTransaction } from "ethers"
 import { ethers, upgrades } from "hardhat"
-import { ExampleToken__factory } from "../../../typechain"
+import { ExampleTokenV2__factory, ExampleToken__factory } from "../../../typechain"
 
 describe("ExampleToken", () => {
   const setup = async (deployer: SignerWithAddress) => {
@@ -12,20 +13,104 @@ describe("ExampleToken", () => {
     return { token }
   }
 
+  it("deploy proxy", async () => {
+    const [owner] = await ethers.getSigners();
+    const { token } = await setup(owner)
+    const [name, symbol, contractVersion, upgradedVersion] = await Promise.all([
+      token.name(),
+      token.symbol(),
+      token.contractVersion(),
+      token.upgradedVersion(),
+    ]);
+    expect(name).to.eq("Example Token")
+    expect(symbol).to.eq("TOKEN")
+    expect(contractVersion.toNumber()).to.eq(1)
+    expect(upgradedVersion.toNumber()).to.eq(1)
+  })
+
   describe("upgradable", () => {
-    it("deploy proxy", async () => {
-      const [owner] = await ethers.getSigners();
-      const { token } = await setup(owner)
-      const [name, symbol, contractVersion, upgradedVersion] = await Promise.all([
-        token.name(),
-        token.symbol(),
-        token.contractVersion(),
-        token.upgradedVersion(),
-      ]);
-      expect(name).to.eq("Example Token")
-      expect(symbol).to.eq("TOKEN")
-      expect(contractVersion.toNumber()).to.eq(1)
-      expect(upgradedVersion.toNumber()).to.eq(1)
+    describe("2nd implementation", () => {
+      it("success", async () => {
+        const [owner] = await ethers.getSigners();
+        const { token } = await setup(owner)
+
+        const [name, symbol, contractVersion, upgradedVersion] = await Promise.all([
+          token.name(),
+          token.symbol(),
+          token.contractVersion(),
+          token.upgradedVersion(),
+        ]);
+
+        // upgrade to v2
+        const upgraded = await upgrades.upgradeProxy(
+          token,
+          new ExampleTokenV2__factory(owner),
+        );
+
+        const [_name, _symbol, _contractVersion, _upgradedVersion] = await Promise.all([
+          upgraded.name(),
+          upgraded.symbol(),
+          upgraded.contractVersion(),
+          upgraded.upgradedVersion(),
+        ]);
+        expect(_name).to.eq(name)
+        expect(_symbol).to.eq(symbol)
+        expect(_contractVersion.toNumber()).to.eq(contractVersion.toNumber())
+        expect(_upgradedVersion.toNumber()).to.eq(upgradedVersion.toNumber())
+      })
+
+      it("success with function", async () => {
+        const [owner] = await ethers.getSigners();
+        const { token } = await setup(owner)
+
+        const [name, symbol] = await Promise.all([
+          token.name(),
+          token.symbol(),
+          token.contractVersion(),
+          token.upgradedVersion(),
+        ]);
+
+        // upgrade to v2
+        const upgraded = await upgrades.upgradeProxy(
+          token,
+          new ExampleTokenV2__factory(owner),
+          { call: { fn: "upgradeNextVersion" } }
+        );
+
+        const [_name, _symbol, _contractVersion, _upgradedVersion] = await Promise.all([
+          upgraded.name(),
+          upgraded.symbol(),
+          upgraded.contractVersion(),
+          upgraded.upgradedVersion(),
+        ]);
+        expect(_name).to.eq(name)
+        expect(_symbol).to.eq(symbol)
+        expect(_contractVersion.toNumber()).to.eq(2)
+        expect(_upgradedVersion.toNumber()).to.eq(2)
+      })
+
+      it("success with function to use upgradable", async () => {
+        const [owner] = await ethers.getSigners();
+        const { token } = await setup(owner)
+
+        // upgrade to v2
+        const upgraded = await upgrades.upgradeProxy(
+          token,
+          new ExampleTokenV2__factory(owner),
+          { call: { fn: "upgradeNextVersionWithUpgradeable" } }
+        );
+
+        const [_name, _symbol, _contractVersion, _upgradedVersion] = await Promise.all([
+          upgraded.name(),
+          upgraded.symbol(),
+          upgraded.contractVersion(),
+          upgraded.upgradedVersion(),
+        ]);
+        expect(_name).to.eq("Example Token V2")
+        expect(_symbol).to.eq("TOKENV2")
+        expect(_contractVersion.toNumber()).to.eq(2)
+        expect(_upgradedVersion.toNumber()).to.eq(2)
+      })
     })
   })
 })
