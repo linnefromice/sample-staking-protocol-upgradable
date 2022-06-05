7b2d88d1d619a0bc9fe65ac070be2bc7687b2fdd
linnefromice <linnefromice@gmail.com>
2022-06-06 00:45:36 +0900

    update: setup gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import '@nomiclabs/hardhat-ethers'
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-import "@openzeppelin/hardhat-upgrades"
+import "@openzeppelin/hardhat-upgrades";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import { HardhatUserConfig } from "hardhat/config";
@@ -51,8 +51,12 @@ const config: HardhatUserConfig = {
     }
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    enabled: true,
     currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_KEY,
+    token: "ETH",
+    showTimeSpent: true,
+    showMethodSig: true
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
