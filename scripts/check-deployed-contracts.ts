445ff05bd80f97c80add4628eea3d2aa06e2eab1
linnefromice <linnefromice@gmail.com>
2022-06-06 12:31:48 +0900

    clean: update check script - addresses in localhost

--- scripts/check-deployed-contracts.ts
+++ scripts/check-deployed-contracts.ts
@@ -9,15 +9,15 @@ const addresses = {
     dai: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
     trueUsd: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
   },
-  pools: {
-    dai: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
-    trueUsd: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9'
-  },
-  stakingPool: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
   tokens: {
-    govToken: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
-    veToken: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'
-  }
+    govToken: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
+    veToken: '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318'
+  },
+  pools: {
+    dai: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
+    trueUsd: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853'
+  },
+  stakingPool: '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e',
 }
 
 type CheckFunctionArgs = {
